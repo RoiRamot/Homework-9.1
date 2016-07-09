fff83ab694d487c2e80f33cb5287a169c4d1680d
roiramot <roir@codevalue.net>
2016-07-09 21:00:33 +0300

    Add
    Add

---
+++ ShapesApp/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShapesLibs;
+
+namespace ShapesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Define Shapes
+           Rectangle shape1 = new Rectangle(10,15);
+           Rectangle shape2 = new Rectangle(20,5);
+           Rectangle shape3 = new Rectangle(5,30);
+           Ellipse shape4 = new Ellipse(5,5);
+           Ellipse shape5 = new Ellipse(9,30);
+           Ellipse shape6 = new Ellipse(99,20);
+           Circle shape7 = new Circle(5);
+           Circle shape8 = new Circle(9);
+           Circle shape9 = new Circle(99);
+
+            //Adding New shapes to list
+            ShapeManager shapeManager = new ShapeManager();
+            shapeManager.Add(shape1);
+            shapeManager.Add(shape2);
+            shapeManager.Add(shape3);
+            shapeManager.Add(shape4);
+            shapeManager.Add(shape5);
+            shapeManager.Add(shape6);
+            shapeManager.Add(shape7);
+            shapeManager.Add(shape8);
+            shapeManager.Add(shape9);
+
+
+
+            Console.WriteLine("Display Method");
+            shapeManager.DisplayAll();
+            
+            Console.WriteLine();
+            Console.WriteLine("Save Method");
+            Console.WriteLine(shapeManager.Save());
+            
+            Console.ReadLine();
+
+
+        }
+
+       public class ShapeManager
+        {
+           ArrayList ShapesList = new ArrayList();
+
+            public void Add(object shape)
+            {
+                ShapesList.Add(shape);
+            }
+
+            public void DisplayAll()
+            {
+                foreach (var obj in ShapesList)
+                {
+                    if (obj is Shape)
+                    {
+                        Shape shape = (Shape)obj;
+                        Console.WriteLine(shape.Area);
+                        shape.Display();
+                    }
+                    
+                }
+            }
+
+
+            public int Count
+            {
+                get { return ShapesList.Count; }
+                private set{}
+            }
+
+            public string Save()
+            {
+                StringBuilder sb= new StringBuilder();
+                foreach (var shape in ShapesList)
+                {
+                    if (shape is IPersist)
+                    {
+                        IPersist p = (IPersist) shape;
+                        p.Write(sb);
+                    }
+                }
+
+                return sb.ToString();
+            }
+
+           public object this[int i]
+           {
+               get { return ShapesList[i]; }
+           }
+        }
+    }
+}
---
+++ ShapesLibs/Class1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesLibs
+{
+
+    public interface IPersist
+    {
+        void Write(StringBuilder sb);
+    }
+   public abstract class Shape
+    {
+        public ConsoleColor Color { get; set; }
+        public abstract int Area { get;}
+
+        public Shape(ConsoleColor color)
+        {
+            Color = color;
+        }
+
+        public Shape()
+        {
+            Color = ConsoleColor.White;
+        }
+
+        public virtual void Display()
+        {
+            Console.ForegroundColor = Color;
+        }
+    }
+
+    public class Rectangle : Shape, IPersist, IComparable
+    {
+
+        public int Height;
+        public int Width;
+        public Rectangle(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public override int Area
+        {
+            get
+            {
+                int area = Height*Width;
+                return area;
+            }
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine(Height);
+            Console.WriteLine(Width);
+        }
+
+        public void Write(StringBuilder sb)
+        {
+            sb.AppendLine(Height.ToString());
+            sb.AppendLine(Width.ToString());
+        }
+
+        public int CompareTo(object obj)
+        {
+            Rectangle otherRectangle = obj as Rectangle;
+            if (otherRectangle!=null)
+            {
+                return this.Area.CompareTo(otherRectangle.Area);
+            }
+            else
+            {
+                throw new Exception("unable to compare");
+            }
+        }
+    }
+
+    public class Ellipse : Shape, IPersist
+    {
+
+        public int Height;
+        public int Width;
+        public Ellipse(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }    
+        public Ellipse()
+        {
+            Height = 10;
+            Width = 10;
+        }
+
+        public override int Area
+        {
+            get
+            {
+
+                int area = Convert.ToInt32(System.Math.PI) * (Height * Width);
+                return area;
+            }
+        }
+
+
+        public virtual void Write(StringBuilder sb)
+        {
+            sb.AppendLine(Height.ToString(CultureInfo.InvariantCulture));
+            sb.AppendLine(Width.ToString(CultureInfo.InvariantCulture));
+        }
+    }
+
+    public class Circle : Ellipse
+    {
+        public int Radius;
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+
+        public override int Area
+        {
+            get
+            {
+
+                int area = Convert.ToInt32(System.Math.PI)*Radius*2;
+                return area;
+            }
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine(Radius);
+        }
+
+        public override void Write(StringBuilder sb)
+        {
+           sb.AppendLine(Radius.ToString(CultureInfo.InvariantCulture));
+        }
+    }
+
+}
